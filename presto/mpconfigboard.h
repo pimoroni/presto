9d0d985a9dfe8d18fff67dfd98a3221084d05efe
Phil Howard <github@gadgetoid.com>
2024-08-21 15:39:37 +0100

    Config: Make Presto wireless sloooowwwww.

--- presto/mpconfigboard.h
+++ presto/mpconfigboard.h
@@ -25,6 +25,10 @@
 #define CYW43_GPIO (1)
 #define CYW43_SPI_PIO (1)
 
+// Make slow for the awesome power!
+#define CYW43_PIO_CLOCK_DIV_INT 150
+#define CYW43_SPI_PROGRAM_NAME spi_gap0_sample1
+
 // For debugging mbedtls - also set
 // Debug level (0-4) 1=warning, 2=info, 3=debug, 4=verbose
 // #define MODUSSL_MBEDTLS_DEBUG_LEVEL 1
