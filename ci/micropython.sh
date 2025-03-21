8a6356e6fbba2c59dd93e50e0d0166d63dff5dc3
Phil Howard <github@gadgetoid.com>
2025-03-21 11:22:58 +0000

    CI: Switch pimoroni-pico to Mike's faster-pv-render branch.

--- ci/micropython.sh
+++ ci/micropython.sh
@@ -3,8 +3,8 @@ export TERM=${TERM:="xterm-256color"}
 MICROPYTHON_FLAVOUR="pimoroni"
 MICROPYTHON_VERSION="feature/presto-wireless-2025"
 
-PIMORONI_PICO_FLAVOUR="pimoroni"
-PIMORONI_PICO_VERSION="feature/picovector2-and-layers"
+PIMORONI_PICO_FLAVOUR="MichaelBell"
+PIMORONI_PICO_VERSION="faster-pv-render"
 
 PY_DECL_VERSION="v0.0.3"
 DIR2UF2_VERSION="v0.0.9"
