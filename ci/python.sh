1493dd80fd63b38fbed1e9b6448b70db96556f1b
Phil Howard <github@gadgetoid.com>
2025-04-01 16:56:48 +0100

    QA: Add bugbears.

--- ci/python.sh
+++ ci/python.sh
@@ -1,19 +1,22 @@
+QA_INCLUDE="F,Q,W,E,B"
+QA_IGNORE="E501,E402"
+
 function qa_prepare_all {
     pip install ruff
 }
 
-function qa_examples_lint {
-    ruff check --select F,Q,W,E examples/ --ignore E501,E402
+function qa_examples_check {
+    ruff check --select "$QA_INCLUDE" examples/ --ignore "$QA_IGNORE"
 }
 
 function qa_examples_fix {
-    ruff check --select F,Q,W,E examples/ --ignore E501,E402 --fix
+    ruff check --select "$QA_INCLUDE" examples/ --ignore "$QA_IGNORE" --fix
 }
 
-function qa_modules_lint {
-    ruff check --select F,Q,W,E examples/ --ignore E501,E402
+function qa_modules_check {
+    ruff check --select "$QA_INCLUDE" examples/ --ignore "$QA_IGNORE"
 }
 
 function qa_modules_fix {
-    ruff check --select F,Q,W,E examples/ --ignore E501,E402 --fix
+    ruff check --select "$QA_INCLUDE" examples/ --ignore "$QA_IGNORE" --fix
 }
