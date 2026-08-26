d47e0abbd9c1b71cbc8b26890ab8c6a020622a33
Phil Howard <github@gadgetoid.com>
2026-08-26 15:19:50 +0100

    presto.py: Expose the PicoVector drawing surface as display.
    display is now a picovector image wrapping the module's RGBA buffer, so
    examples use image drawing calls and colour objects rather than PicoGraphics
    pens. Drops the palette, direct_to_fb and layers arguments, which described
    PicoGraphics buffer layouts that no longer exist.

--- modules/py_frozen/presto.py
+++ modules/py_frozen/presto.py
@@ -4,13 +4,7 @@ from collections import namedtuple
 import _presto
 from ezwifi import EzWiFi
 from machine import PWM, Pin
-from picographics import (
-    DISPLAY_PRESTO,
-    DISPLAY_PRESTO_FULL_RES,
-    PEN_P8,
-    PEN_RGB565,
-    PicoGraphics,
-)
+from picovector import image
 from touch import FT6236
 
 Touch = namedtuple("touch", ("x", "y", "touched"))
@@ -39,7 +33,7 @@ class Presto():
     NUM_LEDS = 7
     LED_PIN = 33
 
-    def __init__(self, full_res=False, palette=False, ambient_light=False, direct_to_fb=False, layers=None, rotate=ROTATE_0):
+    def __init__(self, full_res=False, ambient_light=False, rotate=ROTATE_0):
         # WiFi - *must* happen before Presto bringup
         # Note: Forces WiFi details to be in secrets.py
         self.wifi = EzWiFi()
@@ -47,14 +41,13 @@ class Presto():
         # Touch Input
         self.touch = FT6236(full_res=full_res, rotate=rotate)
 
-        # Display Driver & PicoGraphics
-        if layers is None:
-            layers = 1 if full_res else 2
-        pen = PEN_P8 if palette else PEN_RGB565
-        self.presto = _presto.Presto(full_res=full_res, palette=palette, rotate=rotate)
-        self.buffer = None if (full_res and not palette and not direct_to_fb) else memoryview(self.presto)
-        self.display = PicoGraphics(DISPLAY_PRESTO_FULL_RES if full_res else DISPLAY_PRESTO, buffer=self.buffer, layers=layers, pen_type=pen)
-        self.width, self.height = self.display.get_bounds()
+        self.presto = _presto.Presto(full_res=full_res, rotate=rotate)
+        self.width = 480 if full_res else 240
+        self.height = 480 if full_res else 240
+
+        # The C module hands out its RGBA8888 drawing surface; picovector
+        # rasterises straight into it and update() converts it for scanout.
+        self.display = image(self.width, self.height, memoryview(self.presto))
 
         if ambient_light:
             self.presto.auto_ambient_leds(True)
@@ -93,14 +86,13 @@ class Presto():
         self.touch.poll()
 
     def update(self):
-        self.presto.update(self.display)
+        self.presto.update()
         self.touch.poll()
 
     def partial_update(self, x, y, w, h):
-        self.presto.partial_update(self.display, x, y, w, h)
+        self.presto.partial_update(x, y, w, h)
         self.touch.poll()
 
     def clear(self):
         self.display.clear()
-        self.presto.update(self.display)
-
+        self.presto.update()
