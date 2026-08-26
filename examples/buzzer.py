1da1976a4f8d7cdc9c9ee3ce49b320297a1c3cdf
Phil Howard <github@gadgetoid.com>
2026-08-26 15:39:42 +0100

    examples: Port buzzer to the PicoVector API.
    The old text anchor was the baseline, so centring added half the text height;
    the new one is the top of the em box, so it subtracts.

--- examples/buzzer.py
+++ examples/buzzer.py
@@ -1,10 +1,10 @@
-from picovector import ANTIALIAS_FAST, PicoVector, Polygon, Transform
+from picovector import color, font, shape
 from presto import Buzzer, Presto
 from touch import Button
 
 presto = Presto()
 display = presto.display
-WIDTH, HEIGHT = display.get_bounds()
+WIDTH, HEIGHT = display.width, display.height
 
 # CONSTANTS
 CX = WIDTH // 2
@@ -13,36 +13,26 @@ CY = HEIGHT // 2
 BUTTON_WIDTH = 110
 BUTTON_HEIGHT = 110
 
-# Pico Vector
-vector = PicoVector(display)
-vector.set_antialiasing(ANTIALIAS_FAST)
-t = Transform()
-
-vector.set_font("Roboto-Medium.af", 54)
-vector.set_font_letter_spacing(100)
-vector.set_font_word_spacing(100)
-vector.set_transform(t)
+display.font = font.load("Roboto-Medium.af")
 
 # Couple of colours for use later
-WHITE = display.create_pen(255, 255, 255)
-RED = display.create_pen(230, 60, 45)
-BLACK = display.create_pen(0, 0, 0)
+WHITE = color.rgb(255, 255, 255)
+RED = color.rgb(230, 60, 45)
+BLACK = color.rgb(0, 0, 0)
 
 # We'll need this for the touch element of the screen
 touch = presto.touch
 
 # Define our button vectors and touch button
 button = Button(CX - (BUTTON_WIDTH // 2), CY - (BUTTON_HEIGHT // 2), BUTTON_WIDTH, BUTTON_HEIGHT)
-button_vector = Polygon()
-button_vector.circle(CX, CY, 50)
-button_outline = Polygon()
-button_outline.circle(CX, CY, 54, 5)
+button_vector = shape.circle(CX, CY, 50)
+button_outline = shape.circle(CX, CY, 54).stroke(5)
 
 # Calculate our text positions now rather than in the main loop
-vector.set_font_size(28)
-x, y, w, h = vector.measure_text("TOUCH", x=0, y=0, angle=None)
+TEXT_SIZE = 28
+w, h = display.measure_text("TOUCH", TEXT_SIZE)
 text_x = int(CX - (w // 2))
-text_y = int(CY + (h // 2))
+text_y = int(CY - (h // 2))
 text_x_offset = text_x + 2
 text_y_offset = text_y + 2
 
@@ -55,20 +45,20 @@ while True:
     touch.poll()
 
     # Clear the screen and set the background colour
-    display.set_pen(WHITE)
+    display.pen = WHITE
     display.clear()
 
     # Draw the touch button outline and inner section
-    display.set_pen(BLACK)
-    vector.draw(button_outline)
-    display.set_pen(RED)
-    vector.draw(button_vector)
+    display.pen = BLACK
+    display.shape(button_outline)
+    display.pen = RED
+    display.shape(button_vector)
 
     # Draw vector text with drop shadow
-    display.set_pen(BLACK)
-    vector.text("TOUCH", text_x_offset, text_y_offset)
-    display.set_pen(WHITE)
-    vector.text("TOUCH", text_x, text_y)
+    display.pen = BLACK
+    display.text("TOUCH", text_x_offset, text_y_offset, TEXT_SIZE)
+    display.pen = WHITE
+    display.text("TOUCH", text_x, text_y, TEXT_SIZE)
 
     # If we're pressing the onscreen button, play a tone!
     # otherwise play nothing :)
